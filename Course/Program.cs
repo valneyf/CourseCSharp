fac9fe1eee5455d292cb8b86caa460989d815030
Valney Filho <valneyf@yahoo.com>
2020-11-15 14:37:26 -0300

    Ref. as aulas 43 e 44 - Object e ToString

--- Course/Program.cs
+++ Course/Program.cs
@@ -7,35 +7,36 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Triangulo x, y;
+            Produto p = new Produto();
 
-            x = new Triangulo();
-            y = new Triangulo();
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Entre com as medidas do triângulo X:");
-            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: "+ p);
 
-            Console.WriteLine("Entre com as medidas do triângulo Y:");
-            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser atualizados ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
 
-            double areaX = x.Area();
-            double areaY = y.Area();
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
 
-            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removidos ao estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
 
-            if (areaX > areaY)
-            {
-                Console.WriteLine("Maior área: X");
-            }
-            else
-            {
-                Console.WriteLine("Maior área: Y");
-            }
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+
         }
     }
 }
