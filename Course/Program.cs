bbafd0a3d96ec6027c4e2f1063390051109d11a3
Valney Filho <valneyf@yahoo.com>
2020-11-15 15:25:39 -0300

    Exercicio 1 - Classes, Atributos e Metodos

--- Course/Program.cs
+++ Course/Program.cs
@@ -7,35 +7,15 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Produto p = new Produto();
+            Retangulo ret = new Retangulo();
+            
+            Console.WriteLine("Entre com a largura e altura do retângulo:");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Entre com os dados do produto:");
-            Console.Write("Nome: ");
-            p.Nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Quantidade no estoque: ");
-            p.Quantidade = int.Parse(Console.ReadLine());
-
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto: "+ p);
-
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser atualizados ao estoque: ");
-            int qte = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser removidos ao estoque: ");
-            qte = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-
+            Console.WriteLine($"AREA = {ret.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"PERÍMETRO = {ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"DIAGONAL = {ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
 
         }
     }
