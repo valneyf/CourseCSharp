b3a260b61dbc05a79d758b70bb089067b0ccd448
Valney Filho <valneyf@yahoo.com>
2020-11-25 16:53:56 -0300

    Ref aula 70

--- Course/Program.cs
+++ Course/Program.cs
@@ -9,23 +9,25 @@ namespace Course
         {
             int n = int.Parse(Console.ReadLine());
 
-            // Cria o vetor na memória
-            double[] vect = new double[n];
-
+            Product[] vect = new Product[n];
+                                   
             for (int i = 0; i < n; i++)
             {
-                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price };
             }
 
             double sum = 0.0;
             for (int i = 0; i < n; i++)
             {
-                sum += vect[i];
+                sum += vect[i].Price;
             }
 
             double avg = sum / n;
 
-            Console.WriteLine("AVERAGE HEIGHT: " + avg.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("AVERAGE PRICE: " + avg.ToString("F2",CultureInfo.InvariantCulture));
         }
     }
 }
