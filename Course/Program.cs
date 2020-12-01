01aee1c4f07a8f6c7548bd37ebb5dc85cd9cb537
Valney Filho <valneyf@yahoo.com>
2020-11-30 21:09:24 -0300

    Ref. aula 77

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,40 +1,53 @@
 ﻿using System;
 using System.Globalization;
+using System.Collections.Generic;
 
-namespace Course
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Quantos quartos serão alugados? ");
-            int n = int.Parse(Console.ReadLine());
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            List<string> list = new List<string>();
 
-            Rentail[] vect = new Rentail[10];
-                                  
-            for (int i = 1; i <= n; i++)
-            {
-                Console.WriteLine();
-                Console.WriteLine($"Aluguel #{i}:");
-                Console.Write("Nome: ");
-                string name = Console.ReadLine();
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+            list.Insert(2, "Marco");
 
-                Console.Write("Email: ");
-                string email = Console.ReadLine();
+            foreach (string obj in list) {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("List count: " + list.Count);
 
-                Console.Write("Quarto: ");
-                int room = int.Parse(Console.ReadLine());
-                vect[room] = new Rentail { Name = name, Email = email };
+            // Função lambda é uma função anonima
+            // Dentro do Find, há uma função lambda
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine("First 'A': " + s1);
+
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Last 'A': " + s2);
+
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("First position 'A': " + pos1);
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Last position 'A': " + pos2);
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("------------------------------");
+            foreach (string obj in list2) {
+                Console.WriteLine(obj);
             }
 
-            Console.WriteLine();
-            Console.WriteLine("Quartos ocupados:");
-            for (int i = 0; i < 10; i++)
-            {
-                if (vect[i] != null)
-                {
-                    Console.WriteLine($"{i}: {vect[i].Name}, {vect[i].Email}");
-                }
+            list.Remove("Alex");
+            Console.WriteLine("------------------------------");
+            foreach (string obj in list) {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveAll(x => x[0] == 'M');
+            Console.WriteLine("------------------------------");
+            foreach (string obj in list) {
+                Console.WriteLine(obj);
             }
         }
     }
