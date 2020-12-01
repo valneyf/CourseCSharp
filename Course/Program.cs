3b020cf0d9159e7359c2acc262a5418849d169f4
Valney Filho <valneyf@yahoo.com>
2020-11-30 22:44:33 -0300

    Ref. Exercicio de Fixação aula 78(Listas)

--- Course/Program.cs
+++ Course/Program.cs
@@ -5,48 +5,42 @@ using System.Collections.Generic;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            List<string> list = new List<string>();
+            List<Employee> list = new List<Employee>();
 
-            list.Add("Maria");
-            list.Add("Alex");
-            list.Add("Bob");
-            list.Add("Anna");
-            list.Insert(2, "Marco");
+            Console.Write("How many employees will be registered? ");
+            int record = int.Parse(Console.ReadLine());
 
-            foreach (string obj in list) {
-                Console.WriteLine(obj);
-            }
-            Console.WriteLine("List count: " + list.Count);
+            for (int i = 1; i <= record; i++) {
+                Console.WriteLine($"Employee #{i}:");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
 
-            // Função lambda é uma função anonima
-            // Dentro do Find, há uma função lambda
-            string s1 = list.Find(x => x[0] == 'A');
-            Console.WriteLine("First 'A': " + s1);
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
 
-            string s2 = list.FindLast(x => x[0] == 'A');
-            Console.WriteLine("Last 'A': " + s2);
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            int pos1 = list.FindIndex(x => x[0] == 'A');
-            Console.WriteLine("First position 'A': " + pos1);
-
-            int pos2 = list.FindLastIndex(x => x[0] == 'A');
-            Console.WriteLine("Last position 'A': " + pos2);
-
-            List<string> list2 = list.FindAll(x => x.Length == 5);
-            Console.WriteLine("------------------------------");
-            foreach (string obj in list2) {
-                Console.WriteLine(obj);
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine();
             }
 
-            list.Remove("Alex");
-            Console.WriteLine("------------------------------");
-            foreach (string obj in list) {
-                Console.WriteLine(obj);
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int searchID = int.Parse(Console.ReadLine());
+
+            Employee busca = list.Find(x => x.Id == searchID);
+            if (busca != null) {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                busca.IncreaseSalary(percentage);
+            }
+            else {
+                Console.WriteLine("This id does not exist!");
             }
 
-            list.RemoveAll(x => x[0] == 'M');
-            Console.WriteLine("------------------------------");
-            foreach (string obj in list) {
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            foreach (Employee obj in list) {
                 Console.WriteLine(obj);
             }
         }
