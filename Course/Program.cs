607d12c7fbf1a0be521e743d99d4d08ace96f71f
Valney Filho <valneyf@yahoo.com>
2021-03-27 15:37:40 -0300

    Implementing Extension Methods

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,38 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Course.Entities;
 
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            Dictionary<string, int> election = new Dictionary<string, int>();
+            string s1 = "Good afternoon dear student!";
 
-            Console.Write("Enter file full path: ");
-            string path = Console.ReadLine();
-
-            try {
-                using (StreamReader sr = File.OpenText(path)) {
-                    while (!sr.EndOfStream) {
-                        string[] line = sr.ReadLine().Split(",");
-
-                        if (!election.ContainsKey(line[0])) {
-                            election.Add(line[0], int.Parse(line[1]));
-                        }
-                        else {
-                            election[line[0]] += int.Parse(line[1]);
-                        }
-                    }
-                }
-
-                Console.WriteLine("Result elections: ");
-                foreach (var candidate in election) {
-                    Console.WriteLine(candidate.Key + ": " + candidate.Value);
-                }
-            }
-            catch (IOException e) {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(s1.Cut(10));
+            
         }
     }
 }
